de9759313ae3e618bbfd221322e552e9fd79f4ba
aakshay18 <akshaykadama9@gmail.com>
2023-12-30 19:40:35 +0530

    1st commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='exploring cookiecutter templete and some dvc commands',
+    author='akshay kadam',
+    license='',
+)
